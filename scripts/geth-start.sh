4a950f3f421dddf2db3b047a73347d2ef7e5d317
Daniel Bidulock <daniel@capitolhill.ca>
2018-01-31 17:01:30 -0500

    Enable fast syncing with geth

--- scripts/geth-start.sh
+++ scripts/geth-start.sh
@@ -6,4 +6,4 @@
 # Type `screen -x geth` to view
 # `Ctrl-A-D` to put process in background
 #
-screen -dmS geth /usr/bin/geth --rpc --rpcaddr "0.0.0.0"
+screen -dmS geth /usr/bin/geth --fast --cache=1024 --rpc --rpcaddr "0.0.0.0"
